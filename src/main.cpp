18c8a62b7025698f46b3044fe2425482d2fb118b
Gurdas Satwani <gurdassatwani1@gmail.com>
2025-05-27 10:48:26 +0530

    init

---
+++ src/main.cpp
@@ -0,0 +1,25 @@
+#include <raylib.h>
+
+int main()
+{
+    constexpr float screenW{800};
+    constexpr float screenH{450};
+
+    InitWindow(screenW, screenH, "Asteroids");
+
+    constexpr int targetFPS{60};
+    SetTargetFPS(targetFPS);
+
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+
+        DrawFPS(0, 0);
+        EndDrawing();
+    }
+
+    CloseWindow();
+
+    return 0;
+}
